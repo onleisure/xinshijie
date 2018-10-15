70e53b395c72ca94e423040a887828c94a08da69
zxystop <m15565215802@163.com>
2018-10-15 20:26:10 +0800

    update2

--- src/com/xinshijie/Wuhuang.java
+++ src/com/xinshijie/Wuhuang.java
@@ -3,8 +3,7 @@ package com.xinshijie;
 public class Wuhuang {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("aaaaaaaaaaaaaaaa");
 	}
 
 }
