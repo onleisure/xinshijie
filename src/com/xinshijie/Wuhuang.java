ef01cd31d8524dc7757006594026bbb1c9d67138
zhaoxy <zzzzhaoxy@163.com>
2018-12-23 18:42:17 +0800

    v1.3

--- src/com/xinshijie/Wuhuang.java
+++ src/com/xinshijie/Wuhuang.java
@@ -6,6 +6,7 @@ public class Wuhuang {
 		System.out.println("aaaaaaaaaaaaaaaa");
 		System.out.println("第五个皇帝诞生了");
         System.out.println("被凯多一棒子怼懵了");
-    }
+		System.out.println("痛的不行");
+	}
 
 }
